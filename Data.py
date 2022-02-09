0367bec257ca66de6c22f6af56edb1b8ee198bd7
AIssa HOUDJEDJ <aissa.av@gmail.com>
2022-02-09 20:24:07 +0300

    v2

---
+++ Data.py
@@ -0,0 +1,166 @@
+import argparse
+import numpy as np
+import os
+import pandas as pd
+import statistics
+import matplotlib.pyplot as plt
+from tqdm import tqdm as tqdm
+from numpy import trapz
+from sklearn import metrics
+from matplotlib.ticker import MaxNLocator
+from Data import *
+class Data:
+
+    def __init__(self, cancer,dataset,netowrk):
+
+        print("---- Data ----")
+        print("A) LOAD Data")
+        self.cancer=cancer
+        self.dataset=dataset
+        self.netowrk=netowrk
+        #ppi networks
+        if "COAD" in self.cancer:
+            self.cancer_="COAD"
+        elif "LUAD" in self.cancer:
+            self.cancer_="LUAD"
+        elif "BRCA" in self.cancer:
+            self.cancer_="BRCA"
+        networks={
+            "ST11":"data/string_v11.5_network.csv",
+            "ST10":"data/string_v10.5_network.csv",
+            "DW":"data/dawnrank_network.csv"
+        }
+        ppi_network = networks[netowrk]
+        #load the ppi network
+        self.ppi_network=self.load_PPI(ppi_network)
+        self.mut_dic=self.load_mutation_data(self.cancer,self.dataset,self.ppi_network)
+
+    ##construct mutation dictionary
+    def load_mutation_data(self,cancer,dataset,ppi):
+
+        mutation_matrix = pd.read_csv("data/"+cancer.split("_")[0]+"_"+dataset+"/MUT.csv", index_col=0,sep=",")
+
+        # filter mutation matrix to contain only mutated genes exist in the PPI netowrk
+        mutation_matrix=mutation_matrix.drop(index=[g for g in mutation_matrix.index if g not in ppi])
+
+        #get sample IDs
+        samples = mutation_matrix.columns.tolist()
+
+
+        #get the set of mutated genes for each sample
+        mutations = mutation_matrix.isin([1])
+
+        # save the mutated genes of each sample in a dictionary
+        mut_dic ={}
+        for s in samples:
+            s=s.replace(".",'-')
+            mut_dic[s]= mutations.index[mutations[s] == True].tolist()
+        print("   1) Mutation data loaded for ",len(samples)," samples. ",mutation_matrix.shape)
+        return mut_dic
+
+    def get_mutation_data(self):
+        return self.mut_dic
+
+    ## Load PPI
+    def load_PPI(self,file):
+        ppi_matrix = pd.read_csv(file,sep="\t")
+        ppi = {}
+        for i in range(len(ppi_matrix.index)):
+            ge1 = ppi_matrix.loc[i,'genes1']
+            ge2 = ppi_matrix.loc[i,'genes2']
+            if ge1 not in ppi:
+                ppi[ge1]=set()
+                ppi[ge1].add(ge2)
+            else:
+                ppi[ge1].add(ge2)
+            if ge2 not in ppi:
+                ppi[ge2]=set()
+                ppi[ge2].add(ge1)
+            else:
+                ppi[ge2].add(ge1)
+        return ppi
+
+    #load drug tqrgets reference set data
+    def load_gdsc_depmap_reference_sets(self,cancer):
+
+
+        data="data/reference_sets/{}_DepMap_GDSC2.txt".format(cancer.split("_")[0])
+        gdsc_depmap={}
+        with open(data) as ifile:
+            for line in ifile.readlines():
+                gdsc_depmap[line.split("\t")[0]]= line.strip().split("\t")[1:]
+
+        print("   3)" ,"Reference sets from  [GDSC U DepMap] data loaded")
+        return gdsc_depmap
+
+    #load CGC reference set data
+    def load_cgc_reference_sets(self,cancer):
+
+        cgc_data = pd.read_csv("data/reference_sets/CGC.csv", index_col=0)
+        cgc = cgc_data.index.tolist()
+        print("   1) Loaded ",len(cgc)," drivers from CGC data")
+
+        #identify cgc cancer type specific drivers
+        cgc_cancer_type=set()
+        cgc_keywords={
+            "COAD":["colon","colorectal"],
+            "LUAD":["lung"],
+            "BRCA":["breast"]
+        }
+
+        for driver in cgc_data.index:
+            if pd.isnull(cgc_data["Tumour Types(Somatic)"][driver]):
+                continue
+            for keyword in cgc_keywords[self.cancer_]:
+                if any(value in cgc_data["Tumour Types(Somatic)"][driver].lower() for value in cgc_keywords[self.cancer_]):
+                    #if keyword in cgc_data["Tumour Types(Somatic)"][driver].lower():
+                    cgc_cancer_type.add(driver)
+
+        print("   2) Identified ",len(cgc_cancer_type)," ",cancer," drivers from CGC data")
+        return cgc,cgc_cancer_type
+
+    #load NCG reference set data
+    def load_ncg_reference_sets(self,cancer,cgc):
+        #NCG
+        with open ("data/reference_sets/NCG6_cancergenes.tsv","r") as ifile:
+            ncg=set()
+            ncg_cgc=set()
+
+            ncg_keywords={
+                "COAD":"colorectal",
+                "LUAD":"lung",
+                "BRCA":"breast"
+            }
+
+            for line in ifile.readlines():
+                line=line.split("\t")
+                if ncg_keywords[self.cancer_] in line[4]:
+                    if line[1] in cgc:
+                        ncg_cgc.add(line[1])
+                    ncg.add(line[1])
+
+        print("   3) Loaded ",len(ncg)," drivers from NCG data")
+        print("   4) Identified ",len(ncg_cgc)," drivers from [NCG ∩ CGC] data")
+        return ncg,ncg_cgc
+
+    #load CancerMine reference set data
+    def load_cancermine_reference_sets(self,cancer,cgc):
+        #CancerMine
+        cancermine = set()
+        cancermine_cgc=set()
+        cancermine_keywords={
+            "COAD":"coad",
+            "LUAD":"luad",
+            "BRCA":"breast"
+        }
+        with  open('data/reference_sets/cancermine_{}.txt'.format(cancermine_keywords[self.cancer_])) as ifile:
+            for line in ifile:
+                gene = line.split()
+                if gene[0] in cgc:
+                    cancermine_cgc.add(gene[0])
+                cancermine.add(gene[0])
+
+
+        print("   5) Loaded ",len(cancermine)," drivers from CancerMine data")
+        print("   6) Identified ",len(cancermine_cgc)," drivers from [CancerMIne ∩ CGC] data")
+        return cancermine,cancermine_cgc
--- evaluation.py
+++ evaluation.py
@@ -16,7 +16,7 @@ from tqdm import tqdm as tqdm
 from numpy import trapz
 from sklearn import metrics
 from matplotlib.ticker import MaxNLocator
-
+from Data import *
 
 
 class evaluation:
@@ -34,7 +34,7 @@ class evaluation:
         for m in os.listdir(self.results):
             if ".txt" in m :
                 methods.append(self.results+'/'+m.split('.txt')[0])
-        print(methods)
+
 
         #calculate the top N value
         for ref_version in self.reference_sets:
@@ -47,6 +47,7 @@ class evaluation:
                 elif self.dataset=="CCLE": # if ccle data, the personalized ref. set is the overlap of mutations of sample i with targets of sample i filtered by CGC
                     if i not in reference:
                         continue
+
                     reference_i = set(mut_dic[i]).intersection(set(reference[i])).intersection(set(cgc))
 
                 if len(reference_i) >= 3: # add the current sample to the selected samples for evaluation, if the size of personalized ref set is greater than 2
@@ -68,6 +69,7 @@ class evaluation:
         if dataset == "TCGA":
             ref_i = set(mut_dic[sample_i]).intersection(set(reference))
         elif dataset == "CCLE":
+
             ref_i = set(mut_dic[sample_i]).intersection(set(reference[sample_i])).intersection(set(self.cgc))
         drivers_ref = set(drivers).intersection(set(ref_i))
         precision = len(drivers_ref)/float(k)
@@ -233,207 +235,76 @@ class evaluation:
         plt.show()
         plt.close()
 
-class load_cancer_data:
-
-    def __init__(self, cancer,dataset):
-        print("B) Load Mutation Data")
-        self.cancer=cancer
-        self.dataset=dataset
-        #ppi networks
-        dawnrank_net = 'data/ppi_dawnrank.csv'
-        #String_net = 'data/ppi_string.csv'
-        #load the ppi network
-        self.ppi_network=self.load_PPI(dawnrank_net)
-        self.mut_dic=self.load_mutation_data(self.cancer,self.dataset,self.ppi_network)
-
-    ## Load PPI
-    def load_PPI(self,file):
-        ppi_matrix = pd.read_csv(file,sep="\t")
-        ppi = {}
-        for i in range(len(ppi_matrix.index)):
-            ge1 = ppi_matrix.loc[i,'genes1']
-            ge2 = ppi_matrix.loc[i,'genes2']
-            if ge1 not in ppi:
-                ppi[ge1]=set()
-                ppi[ge1].add(ge2)
-            else:
-                ppi[ge1].add(ge2)
-            if ge2 not in ppi:
-                ppi[ge2]=set()
-                ppi[ge2].add(ge1)
-            else:
-                ppi[ge2].add(ge1)
-        return ppi
-
-    ##construct mutation dictionary
-    def load_mutation_data(self,cancer,dataset,ppi):
-
-        mutation_matrix = pd.read_csv("data/"+cancer+"_"+dataset+"/mutation_data.csv", index_col=0,sep=",")
-
-        # filter mutation matrix to contain only mutated genes exist in the PPI netowrk
-        mutation_matrix=mutation_matrix.drop(index=[g for g in mutation_matrix.index if g not in ppi])
-
-        #get sample IDs
-        samples = mutation_matrix.columns.tolist()
 
 
-        #get the set of mutated genes for each sample
-        mutations = mutation_matrix.isin([1])
-
-        # save the mutated genes of each sample in a dictionary
-        mut_dic ={}
-        for s in samples:
-            s=s.replace(".",'-')
-            mut_dic[s]= mutations.index[mutations[s] == True].tolist()
-        print("   2) Mutation data loaded for ",len(samples)," samples. ",mutation_matrix.shape)
-        return mut_dic
-
-    def get_mutation_data(self):
-        return self.mut_dic
 
 class ccle_evaluation:
 
-    def __init__(self, cancer,dataset):
+    def __init__(self, cancer,dataset,netowrk):
         self.cancer = cancer
+        self.netowrk = netowrk
         self.dataset=dataset
+        DataC=Data(cancer,dataset,netowrk)
         print("A) Load Refernce Data")
-        self.cgc=self.load_cgc_reference_sets(cancer)
-        drug_targets=self.load_gdsc_depmap_reference_sets(cancer)
+        self.cgc,self.cgc_specific=DataC.load_cgc_reference_sets(cancer)
+        drug_targets=DataC.load_gdsc_depmap_reference_sets(cancer)
 
 
         #laod mutation dataset
-        mut_dic=load_cancer_data(cancer,dataset).get_mutation_data()
+        mut_dic=DataC.get_mutation_data()
         self.reference_sets={"GDSC U DepMap":drug_targets}
-        self.results="results/"+self.dataset +"/"+self.cancer
+        self.results="results/"+self.dataset +"/"+self.cancer+"_"+self.netowrk
 
         evaluation(mut_dic,self.reference_sets,self.cancer,self.dataset,self.results,cgc=self.cgc)
 
-    #load CGC reference set data
-    def load_cgc_reference_sets(self,canccle_evaluationcer):
-        cgc_data = pd.read_csv("data/reference_sets/CGC.csv", index_col=0)
-        cgc = cgc_data.index.tolist()
-        print("   1) Identified ",len(cgc)," drivers from CGC data")
-        return cgc
 
-    #load drug tqrgets reference set data
-    def load_gdsc_depmap_reference_sets(self,cancer):
-
-        if cancer == "COAD":
-            data="data/reference_sets/COAD_DepMap_GDSC2.txt"
-        elif cancer == "LUAD":
-            data="data/reference_sets/LUAD_DepMap_GDSC2.txt"
-        gdsc_depmap={}
-        with open(data) as ifile:
-            for line in ifile.readlines():
-                gdsc_depmap[line.split("\t")[0]]= line.strip().split("\t")[1:]
-
-        print("   2)" ,"Reference sets from  [GDSC U DepMap] data loaded")
-        return gdsc_depmap
 
 class tcga_evaluation:
 
-    def __init__(self, cancer,dataset):
+    def __init__(self, cancer,dataset,netowrk):
+
         self.cancer = cancer
+        self.netowrk=netowrk
+        if "COAD" in self.cancer:
+            self.cancer_="COAD"
+        elif "LUAD" in self.cancer:
+            self.cancer_="LUAD"
+        elif "BRCA" in self.cancer:
+            self.cancer_="BRCA"
+        DataC=Data(cancer,dataset,netowrk)
         self.dataset=dataset
-        print("A) Load Refernce Data")
-        cgc,cgc_cancer_type=self.load_cgc_reference_sets(cancer)
-        ncg,ncg_cgc=self.load_ncg_reference_sets(cancer,cgc)
-        cancermine,cancermine_cgc=self.load_cancermine_reference_sets(cancer,cgc)
+        print("B) Load Refernce Data")
+        cgc,cgc_cancer_type=DataC.load_cgc_reference_sets(cancer)
+        ncg,ncg_cgc=DataC.load_ncg_reference_sets(cancer,cgc)
+        cancermine,cancermine_cgc=DataC.load_cancermine_reference_sets(cancer,cgc)
 
         #laod mutation dataset
-        mut_dic=load_cancer_data(cancer,dataset).get_mutation_data()
+        mut_dic=DataC.get_mutation_data()
         self.reference_sets={ "CGC":cgc_cancer_type,
                             "NCG":ncg_cgc,
                             "CancerMine":cancermine_cgc,
                             "All_CGC":cgc,
                             "All_NCG":ncg,
                             "All_cancermine":cancermine}
-        self.results="results/"+self.dataset +"/"+self.cancer
+        self.results="results/"+self.dataset +"/"+self.cancer+"_"+self.netowrk
 
         evaluation(mut_dic,self.reference_sets,self.cancer,self.dataset,self.results)
 
-    #load CGC reference set data
-    def load_cgc_reference_sets(self,cancer):
 
-        cgc_data = pd.read_csv("data/reference_sets/CGC.csv", index_col=0)
-        cgc = cgc_data.index.tolist()
-        print("   1) Loaded ",len(cgc)," drivers from CGC data")
-
-        #identify cgc cancer type specific drivers
-        cgc_cancer_type=set()
-        if cancer == "COAD":
-            for driver in cgc_data.index:
-                if pd.isnull(cgc_data["Tumour Types(Somatic)"][driver]):
-                    continue
-                if "colon" in cgc_data["Tumour Types(Somatic)"][driver] or "colorectal" in cgc_data["Tumour Types(Somatic)"][driver]:
-                    cgc_cancer_type.add(driver)
-        elif cancer == "LUAD":
-            for driver in cgc_data.index:
-                if pd.isnull(cgc_data["Tumour Types(Somatic)"][driver]):
-                    continue
-                if "lung" in cgc_data["Tumour Types(Somatic)"][driver]:
-                    cgc_cancer_type.add(driver)
-        print("   2) Identified ",len(cgc_cancer_type)," ",cancer," drivers from CGC data")
-        return cgc,cgc_cancer_type
-
-    #load NCG reference set data
-    def load_ncg_reference_sets(self,cancer,cgc):
-        #NCG
-        with open ("data/reference_sets/NCG6_cancergenes.tsv","r") as ifile:
-            ncg=set()
-            ncg_cgc=set()
-            for line in ifile.readlines():
-                line=line.split("\t")
-                if cancer=="COAD":
-                    if "colorectal" in line[4]:
-                        if line[1] in cgc:
-                            ncg_cgc.add(line[1])
-                        ncg.add(line[1])
-                elif cancer=="LUAD":
-                    if "lung" in line[4]:
-                        if line[1] in cgc:
-                            ncg_cgc.add(line[1])
-                        ncg.add(line[1])
-
-        print("   3) Loaded ",len(ncg)," drivers from NCG data")
-        print("   4) Identified ",len(ncg_cgc)," drivers from [NCG ∩ CGC] data")
-        return ncg,ncg_cgc
-
-    #load CancerMine reference set data
-    def load_cancermine_reference_sets(self,cancer,cgc):
-        #CancerMine
-        cancermine = set()
-        cancermine_cgc=set()
-        if cancer=="COAD":
-            with  open('data/reference_sets/cancermine_coad.txt') as ifile:
-                for line in ifile:
-                    gene = line.split()
-                    if gene[0] in cgc:
-                        cancermine_cgc.add(gene[0])
-                    cancermine.add(gene[0])
-        elif cancer=="LUAD":
-             with  open('data/reference_sets/cancermine_luad.txt','r') as ifile:
-                for line in ifile:
-                    gene = line.split()
-                    if gene[0] in cgc:
-                        cancermine_cgc.add(gene[0])
-                    cancermine.add(gene[0])
-
-        print("   5) Loaded ",len(cancermine)," drivers from CancerMine data")
-        print("   6) Identified ",len(cancermine_cgc)," drivers from [CancerMIne ∩ CGC] data")
-        return cancermine,cancermine_cgc
 
 if __name__ == "__main__":
 
     description="Evaluation Framewok"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-d', "--dataset", type=str, required=False, default='TCGA', help="Dataset: TCGA or CCLE")
-    parser.add_argument('-c', '--cancer', type=str, required=False, default="COAD", help="Cancer Type")
+    parser.add_argument('-d', "--dataset", type=str, required=True, default='TCGA', help="Dataset: TCGA or CCLE")
+    parser.add_argument('-c', '--cancer', type=str, required=True, default="COAD", help="Cancer Type")
+    parser.add_argument('-n', '--network', type=str, required=False, default="ST11", help="PPI Network")
 
     args = parser.parse_args()
     dataset = args.dataset
     cancer = args.cancer
+    network = args.network
     if dataset=="TCGA":
-        tcga_evaluation(cancer,dataset)
+        tcga_evaluation(cancer,dataset,network)
     elif dataset=="CCLE":
-        ccle_evaluation(cancer,dataset)
+        ccle_evaluation(cancer,dataset,network)
